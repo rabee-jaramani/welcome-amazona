dc02c2dcbe6db10c0a94c7e7d2aed39792bc01df
Rabee Aljaramani <rabeejermani@gmail.com>
2022-03-08 15:24:44 +0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>
+          Please note that this e-commerce application is using free plans, so
+          if you got an error on first loading, please be patient and try to
+          refresh the page, and its not fully responsive on mobile😋
+        </h2>
+        <div className="btn">
+          <a href="https://rab.vercel.app/" rel="noreferrer">
+            Open Next E-commerce
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
