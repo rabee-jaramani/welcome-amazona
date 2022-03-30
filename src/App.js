d8319a0116804311c97e9a2668eca01394851a07
Rabee Aljaramani <rabeejermani@gmail.com>
2022-03-30 10:52:52 +0400

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
           if you got an error on first loading, please be patient and try to
           refresh the page, and its not fully responsive on mobile😋
         </h2>
+        <h2>You can log in using these credentials:</h2>
+        <h2>User Name: rabee@gmail.com</h2>
+        <h2>Password: 123456</h2>
         <div className="btn">
           <a href="https://rab.vercel.app/" rel="noreferrer">
             Open Next E-commerce
